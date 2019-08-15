88a28727310cc481405882cfa9a62d047659f0ea
jxa8hst <jean_altidor@homdepot.com>
2019-08-14 20:32:20 -0400

    server and web working

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {add} from '@jean/common'
-console.log(add)
+
+
+const INITIAL_STATE = {
+  loading: null,
+  rockets: []
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'LOADING_COMPLETE':
+      return {
+        ...state,
+        ...action.payload,
+        loading: false
+      }
+  
+    default:
+     return state
+  }
+}
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+  useEffect(() => {
+
+  },[])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          {add(1,3)}
         </p>
         <a
           className="App-link"
